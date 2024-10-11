432ebacb2abf1f558eb75c94ece730065ac06afd
tiffany <tiffanylumengkewas@gmail.com>
2024-10-11 13:33:00 +0700

    masih rusak

--- src/components/Pages/ProductSearch/Product-card.jsx
+++ src/components/Pages/ProductSearch/Product-card.jsx
@@ -1,17 +1,21 @@
 import PropTypes from "prop-types";
 import "./Product-card.css";
 import mascot from "../../../assets/mascot-3.png";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const ProductCard = ({product}) => {
+const ProductCard = ({ product }) => {
+  const renderStars = () => {
+    return "⭐️".repeat(product.rating);
+  };
+
   return (
     <Link to={`/product/${product.id}`}>
       <div className="product-card">
         <div className="product-image">
-          <img src={product.image} alt={product.name} />
+          <img src={product.image1} alt={product.name} />
         </div>
         <div className="rating">
-          <span>⭐️⭐️⭐️⭐️⭐️</span>
+          <span>{renderStars()}</span>
           <span>{product.sold}</span>
         </div>
         <div className="product-details">
@@ -34,10 +38,11 @@ const ProductCard = ({product}) => {
 ProductCard.propTypes = {
   product: PropTypes.shape({
     id: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
+    image1: PropTypes.string.isRequired, // Updated to image1
     name: PropTypes.string.isRequired,
     sold: PropTypes.string.isRequired,
     price: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired, // Added rating prop
   }).isRequired,
 };
 
